3a2d7faa1ed305860a2022b1deb73ed43bbb1f30
Filip Paulů <ing.fenix@seznam.cz>
2022-11-15 02:06:27 +0100

    Add ThemeProvider to app

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
+import { ThemeProvider } from './Theme';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById("rpg-note") as HTMLElement);
 root.render(<React.StrictMode>
-  <App />
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>
 </React.StrictMode>);
