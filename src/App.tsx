0264c2cf3a4105050ce9d5a002d1bcaf2940eb64
Filip Paulů <ing.fenix@seznam.cz>
2022-11-17 16:12:52 +0100

    Implement languages to application

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import { ButtonIcon, Flex } from './Components';
 import packageInfo from "../package.json";
 import { checkLoadFile, EMPTY_SAVE, getDataForSave, StorageKey, updateCollection, useStoredState } from './setvices';
 import { useColors, useTheme } from './Theme';
+import { L, useLang, useLanguage } from './Language';
 
 enum Menu {
   File,
@@ -31,7 +32,8 @@ const PICTURE_SIZE = { width: 200, height: 250 };
 
 export default function App() {
   const { theme, setTheme } = useTheme();
-  const colors = useColors();
+  const { language, setLanguage } = useLanguage();
+  const l = useLang();
 
   const fileInput = useRef<null | HTMLInputElement>(null);
   const [menu, setMenu] = useState<null | Menu>(null);
@@ -125,7 +127,7 @@ export default function App() {
         <Flex row align="center">
           <img src="/logo192.png" alt="RPG note logo" style={{ height: "48px", width: "48px" }} />
           <h1>
-            RPG note
+            <L k="title" />
             <span style={{ paddingLeft: "8px", fontSize: "16px", opacity: 0.5 }}>v{packageInfo.version}</span>
           </h1>
         </Flex>
@@ -154,50 +156,50 @@ export default function App() {
       <Split style={{ display: "flex", flexDirection: "column", flexGrow: 1 }} direction="vertical">
         <Flex grow={1} style={{ position: "relative" }}>
           <Flex row style={{ position: "absolute", zIndex: 6, top: "4px", right: "4px" }}>
-            <ButtonIcon icon={mdiCog} text="Soubor" open={menu === Menu.File} active={menu === Menu.File} onClick={() => setMenu(menu === Menu.File ? null : Menu.File)} />
+            <ButtonIcon icon={mdiCog} text={l("file")} open={menu === Menu.File} active={menu === Menu.File} onClick={() => setMenu(menu === Menu.File ? null : Menu.File)} />
             <div style={{ width: "8px" }} />
-            <ButtonIcon icon={mdiEarth} text="Jazyk" open={menu === Menu.Language} active={menu === Menu.Language} onClick={() => setMenu(menu === Menu.Language ? null : Menu.Language)} />
+            <ButtonIcon icon={mdiEarth} text={l("language")} open={menu === Menu.Language} active={menu === Menu.Language} onClick={() => setMenu(menu === Menu.Language ? null : Menu.Language)} />
             <div style={{ width: "8px" }} />
-            <ButtonIcon icon={mdiPalette} text="Styl" open={menu === Menu.Style} active={menu === Menu.Style} onClick={() => setMenu(menu === Menu.Style ? null : Menu.Style)} />
+            <ButtonIcon icon={mdiPalette} text={l("style")} open={menu === Menu.Style} active={menu === Menu.Style} onClick={() => setMenu(menu === Menu.Style ? null : Menu.Style)} />
           </Flex>
 
           <Flex align="flex-end" style={{ position: "absolute", zIndex: 15, top: "52px", right: "104px", height: menu === Menu.File ? "162px" : "0px", overflow: "hidden", minWidth: "152px", transition: "0.3s" }}>
-            <ButtonIcon open icon={mdiFloppy} text="Uložit" onClick={handleSave} />
+            <ButtonIcon open icon={mdiFloppy} text={l("save")} onClick={handleSave} />
             <div style={{ height: "8px" }} />
-            <ButtonIcon open icon={mdiUpload} text="Načíst" onClick={handleLoadDialog} />
+            <ButtonIcon open icon={mdiUpload} text={l("load")} onClick={handleLoadDialog} />
             <div style={{ height: "8px" }} />
-            <ButtonIcon open icon={mdiDeleteOutline} text="Vše smazat" onClick={handleDelete} />
+            <ButtonIcon open icon={mdiDeleteOutline} text={l("removeAll")} onClick={handleDelete} />
           </Flex>
 
           <Flex align="flex-end" style={{ position: "absolute", zIndex: 15, top: "52px", right: "52px", height: menu === Menu.Language ? "162px" : "0px", overflow: "hidden", minWidth: "152px", transition: "0.3s" }}>
-            <ButtonIcon open text="Čeština" onClick={() => { }} />
+            <ButtonIcon open text={l("czech")} active={language === "cs"} onClick={() => setLanguage("cs")} />
             <div style={{ height: "8px" }} />
-            <ButtonIcon open text="English" onClick={() => { }} />
+            <ButtonIcon open text={l("english")} active={language === "en"} onClick={() => setLanguage("en")} />
           </Flex>
 
           <Flex align="flex-end" style={{ position: "absolute", zIndex: 15, top: "52px", right: "4px", height: menu === Menu.Style ? "162px" : "0px", overflow: "hidden", minWidth: "152px", transition: "0.3s" }}>
-            <ButtonIcon open icon={mdiBrightness5} text="Světlá" active={theme === "light"} onClick={() => setTheme("light")} />
+            <ButtonIcon open icon={mdiBrightness5} text={l("light")} active={theme === "light"} onClick={() => setTheme("light")} />
             <div style={{ height: "8px" }} />
-            <ButtonIcon open icon={mdiBrightness7} text="Tmavá" active={theme === "dark"} onClick={() => setTheme("dark")} />
+            <ButtonIcon open icon={mdiBrightness7} text={l("dark")} active={theme === "dark"} onClick={() => setTheme("dark")} />
             <div style={{ height: "8px" }} />
-            <ButtonIcon open icon={mdiPaperRollOutline} text="Pergamen" active={theme === "parchment"} onClick={() => setTheme("parchment")} />
+            <ButtonIcon open icon={mdiPaperRollOutline} text={l("parchment")} active={theme === "parchment"} onClick={() => setTheme("parchment")} />
           </Flex>
 
-          {page === Page.Skills && <Content title="Dovednosti" storageKey={StorageKey.Skills} reload={reload} />}
-          {page === Page.Equipment && <Content title="Vybavení" storageKey={StorageKey.Equipment} reload={reload} />}
-          {page === Page.Note && <Content title="Poznámky" storageKey={StorageKey.Note} reload={reload} />}
+          {page === Page.Skills && <Content title={l("skills")} storageKey={StorageKey.Skills} reload={reload} />}
+          {page === Page.Equipment && <Content title={l("equipment")} storageKey={StorageKey.Equipment} reload={reload} />}
+          {page === Page.Note && <Content title={l("note")} storageKey={StorageKey.Note} reload={reload} />}
 
           <Flex align="flex-end" style={{ position: "absolute", zIndex: 5, bottom: "16px", right: "16px" }}>
-            <ButtonIcon icon={mdiArmFlex} text="Dovednosti" active={page === Page.Skills} onClick={() => setPage(Page.Skills)} />
+            <ButtonIcon icon={mdiArmFlex} text={l("skills")} active={page === Page.Skills} onClick={() => setPage(Page.Skills)} />
             <div style={{ height: "8px" }} />
-            <ButtonIcon icon={mdiSword} text="Vybavení" active={page === Page.Equipment} onClick={() => setPage(Page.Equipment)} />
+            <ButtonIcon icon={mdiSword} text={l("equipment")} active={page === Page.Equipment} onClick={() => setPage(Page.Equipment)} />
             <div style={{ height: "8px" }} />
-            <ButtonIcon icon={mdiFountainPen} text="Poznámky" active={page === Page.Note} onClick={() => setPage(Page.Note)} />
+            <ButtonIcon icon={mdiFountainPen} text={l("note")} active={page === Page.Note} onClick={() => setPage(Page.Note)} />
           </Flex>
         </Flex>
         <Split style={{ display: "flex", flexGrow: 1 }} direction="horizontal">
-          <Content title="Postavy" storageKey={StorageKey.Characters} reload={reload} />
-          <Content title="Akční poznámky" storageKey={StorageKey.ActionNote} reload={reload} />
+          <Content title={l("characters")} storageKey={StorageKey.Characters} reload={reload} />
+          <Content title={l("actionNote")} storageKey={StorageKey.ActionNote} reload={reload} />
         </Split>
       </Split>
     </Flex>
@@ -216,7 +218,7 @@ function Content({ storageKey, title, reload, ...editorProps }: { storageKey: St
 
   return <Flex grow={1} basis={100}>
     {title && <h3 style={{ paddingLeft: "8px", margin: "2px" }}>{title}</h3>}
-    <Flex grow={1} style={{ backgroundColor: colors.backgroundPrimary, borderRadius: "16px 16px 0px 0px" }}>
+    <Flex grow={1} style={{ background: colors.backgroundPrimary, borderRadius: "16px 16px 0px 0px" }}>
       <Editor {...editorProps} value={text} onChange={setText} />
     </Flex>
   </Flex>
