ddb868ec848cd7789123064ddd977b52dad6cfa9
Filip Paulů <ing.fenix@seznam.cz>
2022-11-17 16:12:36 +0100

    Add languages to whole app

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
+import { LanguageProvider } from './Language';
 import { ThemeProvider } from './Theme';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById("rpg-note") as HTMLElement);
 root.render(<React.StrictMode>
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>
+  <LanguageProvider>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </LanguageProvider>
 </React.StrictMode>);
